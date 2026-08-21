22c57826e5cfbfbe1f585bf93ba0125d6a6aa12b
md amaan sahzada <md.25scs2040002270@iilm.edu>
2026-08-21 11:26:32 +0530

    Time: 0 ms (100%), Space: 14.9 MB (94.89%) - LeetHub

---
+++ 2574-left-and-right-sum-differences/2574-left-and-right-sum-differences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+           int total = 0, left = 0;
+
+        for (int x : nums)
+            total += x;
+
+        vector<int> ans;
+
+        for (int x : nums) {
+            total -= x;
+            ans.push_back(abs(left - total));
+            left += x;
+        }
+
+        return ans;
+    }
+};
